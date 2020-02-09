ff54e42f60a0d37acf24576879f2ddbdbd9d5943
Willy Njundong <njundongw@gmail.com>
2020-02-08 20:13:18 -0500

    menu route routes adjusted
    overview and transactions routes display correctly in address bar and menu bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,61 +1,59 @@
-import { ExtraOptions, RouterModule, Routes } from '@angular/router';
-import { NgModule } from '@angular/core';
+import { ExtraOptions, RouterModule, Routes } from "@angular/router";
+import { NgModule } from "@angular/core";
 import {
   NbAuthComponent,
   NbLoginComponent,
   NbLogoutComponent,
   NbRegisterComponent,
   NbRequestPasswordComponent,
-  NbResetPasswordComponent,
-} from '@nebular/auth';
+  NbResetPasswordComponent
+} from "@nebular/auth";
 
 const routes: Routes = [
   {
-    path: 'pages',
-    loadChildren: () => import('app/pages/pages.module')
-      .then(m => m.PagesModule),
+    path: "",
+    loadChildren: () =>
+      import("app/pages/pages.module").then(m => m.PagesModule)
   },
   {
-    path: 'auth',
+    path: "auth",
     component: NbAuthComponent,
     children: [
       {
-        path: '',
-        component: NbLoginComponent,
+        path: "",
+        component: NbLoginComponent
       },
       {
-        path: 'login',
-        component: NbLoginComponent,
+        path: "login",
+        component: NbLoginComponent
       },
       {
-        path: 'register',
-        component: NbRegisterComponent,
+        path: "register",
+        component: NbRegisterComponent
       },
       {
-        path: 'logout',
-        component: NbLogoutComponent,
+        path: "logout",
+        component: NbLogoutComponent
       },
       {
-        path: 'request-password',
-        component: NbRequestPasswordComponent,
+        path: "request-password",
+        component: NbRequestPasswordComponent
       },
       {
-        path: 'reset-password',
-        component: NbResetPasswordComponent,
-      },
-    ],
-  },
-  { path: '', redirectTo: 'pages', pathMatch: 'full' },
-  { path: '**', redirectTo: 'pages' },
+        path: "reset-password",
+        component: NbResetPasswordComponent
+      }
+    ]
+  }
+  //{ path: "**", redirectTo: "" }
 ];
 
 const config: ExtraOptions = {
-  useHash: false,
+  useHash: false
 };
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, config)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -1,247 +1,247 @@
-import { NbMenuItem } from '@nebular/theme';
+import { NbMenuItem } from "@nebular/theme";
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'E-commerce',
-    icon: 'shopping-cart-outline',
-    link: '/pages/dashboard',
-    home: true,
+    title: "Overview",
+    icon: "shopping-cart-outline",
+    link: "/",
+    home: true
   },
   {
-    title: 'IoT Dashboard',
-    icon: 'home-outline',
-    link: '/pages/iot-dashboard',
+    title: "Transactions",
+    icon: "home-outline",
+    link: "/transactions"
   },
   {
-    title: 'FEATURES',
-    group: true,
+    title: "FEATURES",
+    group: true
   },
   {
-    title: 'Layout',
-    icon: 'layout-outline',
+    title: "Layout",
+    icon: "layout-outline",
     children: [
       {
-        title: 'Stepper',
-        link: '/pages/layout/stepper',
+        title: "Stepper",
+        link: "/layout/stepper"
       },
       {
-        title: 'List',
-        link: '/pages/layout/list',
+        title: "List",
+        link: "/layout/list"
       },
       {
-        title: 'Infinite List',
-        link: '/pages/layout/infinite-list',
+        title: "Infinite List",
+        link: "/layout/infinite-list"
       },
       {
-        title: 'Accordion',
-        link: '/pages/layout/accordion',
+        title: "Accordion",
+        link: "/layout/accordion"
       },
       {
-        title: 'Tabs',
-        pathMatch: 'prefix',
-        link: '/pages/layout/tabs',
-      },
-    ],
+        title: "Tabs",
+        pathMatch: "prefix",
+        link: "/layout/tabs"
+      }
+    ]
   },
   {
-    title: 'Forms',
-    icon: 'edit-2-outline',
+    title: "Forms",
+    icon: "edit-2-outline",
     children: [
       {
-        title: 'Form Inputs',
-        link: '/pages/forms/inputs',
+        title: "Form Inputs",
+        link: "/forms/inputs"
       },
       {
-        title: 'Form Layouts',
-        link: '/pages/forms/layouts',
+        title: "Form Layouts",
+        link: "/forms/layouts"
       },
       {
-        title: 'Buttons',
-        link: '/pages/forms/buttons',
+        title: "Buttons",
+        link: "/forms/buttons"
       },
       {
-        title: 'Datepicker',
-        link: '/pages/forms/datepicker',
-      },
-    ],
+        title: "Datepicker",
+        link: "/forms/datepicker"
+      }
+    ]
   },
   {
-    title: 'UI Features',
-    icon: 'keypad-outline',
-    link: '/pages/ui-features',
+    title: "UI Features",
+    icon: "keypad-outline",
+    link: "/ui-features",
     children: [
       {
-        title: 'Grid',
-        link: '/pages/ui-features/grid',
+        title: "Grid",
+        link: "/ui-features/grid"
       },
       {
-        title: 'Icons',
-        link: '/pages/ui-features/icons',
+        title: "Icons",
+        link: "/ui-features/icons"
       },
       {
-        title: 'Typography',
-        link: '/pages/ui-features/typography',
+        title: "Typography",
+        link: "/ui-features/typography"
       },
       {
-        title: 'Animated Searches',
-        link: '/pages/ui-features/search-fields',
-      },
-    ],
+        title: "Animated Searches",
+        link: "/ui-features/search-fields"
+      }
+    ]
   },
   {
-    title: 'Modal & Overlays',
-    icon: 'browser-outline',
+    title: "Modal & Overlays",
+    icon: "browser-outline",
     children: [
       {
-        title: 'Dialog',
-        link: '/pages/modal-overlays/dialog',
+        title: "Dialog",
+        link: "/modal-overlays/dialog"
       },
       {
-        title: 'Window',
-        link: '/pages/modal-overlays/window',
+        title: "Window",
+        link: "/modal-overlays/window"
       },
       {
-        title: 'Popover',
-        link: '/pages/modal-overlays/popover',
+        title: "Popover",
+        link: "/modal-overlays/popover"
       },
       {
-        title: 'Toastr',
-        link: '/pages/modal-overlays/toastr',
+        title: "Toastr",
+        link: "/modal-overlays/toastr"
       },
       {
-        title: 'Tooltip',
-        link: '/pages/modal-overlays/tooltip',
-      },
-    ],
+        title: "Tooltip",
+        link: "/modal-overlays/tooltip"
+      }
+    ]
   },
   {
-    title: 'Extra Components',
-    icon: 'message-circle-outline',
+    title: "Extra Components",
+    icon: "message-circle-outline",
     children: [
       {
-        title: 'Calendar',
-        link: '/pages/extra-components/calendar',
+        title: "Calendar",
+        link: "/extra-components/calendar"
       },
       {
-        title: 'Progress Bar',
-        link: '/pages/extra-components/progress-bar',
+        title: "Progress Bar",
+        link: "/extra-components/progress-bar"
       },
       {
-        title: 'Spinner',
-        link: '/pages/extra-components/spinner',
+        title: "Spinner",
+        link: "/extra-components/spinner"
       },
       {
-        title: 'Alert',
-        link: '/pages/extra-components/alert',
+        title: "Alert",
+        link: "/extra-components/alert"
       },
       {
-        title: 'Calendar Kit',
-        link: '/pages/extra-components/calendar-kit',
+        title: "Calendar Kit",
+        link: "/extra-components/calendar-kit"
       },
       {
-        title: 'Chat',
-        link: '/pages/extra-components/chat',
-      },
-    ],
+        title: "Chat",
+        link: "/extra-components/chat"
+      }
+    ]
   },
   {
-    title: 'Maps',
-    icon: 'map-outline',
+    title: "Maps",
+    icon: "map-outline",
     children: [
       {
-        title: 'Google Maps',
-        link: '/pages/maps/gmaps',
+        title: "Google Maps",
+        link: "/maps/gmaps"
       },
       {
-        title: 'Leaflet Maps',
-        link: '/pages/maps/leaflet',
+        title: "Leaflet Maps",
+        link: "/maps/leaflet"
       },
       {
-        title: 'Bubble Maps',
-        link: '/pages/maps/bubble',
+        title: "Bubble Maps",
+        link: "/maps/bubble"
       },
       {
-        title: 'Search Maps',
-        link: '/pages/maps/searchmap',
-      },
-    ],
+        title: "Search Maps",
+        link: "/maps/searchmap"
+      }
+    ]
   },
   {
-    title: 'Charts',
-    icon: 'pie-chart-outline',
+    title: "Charts",
+    icon: "pie-chart-outline",
     children: [
       {
-        title: 'Echarts',
-        link: '/pages/charts/echarts',
+        title: "Echarts",
+        link: "/charts/echarts"
       },
       {
-        title: 'Charts.js',
-        link: '/pages/charts/chartjs',
+        title: "Charts.js",
+        link: "/charts/chartjs"
       },
       {
-        title: 'D3',
-        link: '/pages/charts/d3',
-      },
-    ],
+        title: "D3",
+        link: "/charts/d3"
+      }
+    ]
   },
   {
-    title: 'Editors',
-    icon: 'text-outline',
+    title: "Editors",
+    icon: "text-outline",
     children: [
       {
-        title: 'TinyMCE',
-        link: '/pages/editors/tinymce',
+        title: "TinyMCE",
+        link: "/editors/tinymce"
       },
       {
-        title: 'CKEditor',
-        link: '/pages/editors/ckeditor',
-      },
-    ],
+        title: "CKEditor",
+        link: "/editors/ckeditor"
+      }
+    ]
   },
   {
-    title: 'Tables & Data',
-    icon: 'grid-outline',
+    title: "Tables & Data",
+    icon: "grid-outline",
     children: [
       {
-        title: 'Smart Table',
-        link: '/pages/tables/smart-table',
+        title: "Smart Table",
+        link: "/tables/smart-table"
       },
       {
-        title: 'Tree Grid',
-        link: '/pages/tables/tree-grid',
-      },
-    ],
+        title: "Tree Grid",
+        link: "/tables/tree-grid"
+      }
+    ]
   },
   {
-    title: 'Miscellaneous',
-    icon: 'shuffle-2-outline',
+    title: "Miscellaneous",
+    icon: "shuffle-2-outline",
     children: [
       {
-        title: '404',
-        link: '/pages/miscellaneous/404',
-      },
-    ],
+        title: "404",
+        link: "/miscellaneous/404"
+      }
+    ]
   },
   {
-    title: 'Auth',
-    icon: 'lock-outline',
+    title: "Auth",
+    icon: "lock-outline",
     children: [
       {
-        title: 'Login',
-        link: '/auth/login',
+        title: "Login",
+        link: "/auth/login"
       },
       {
-        title: 'Register',
-        link: '/auth/register',
+        title: "Register",
+        link: "/auth/register"
       },
       {
-        title: 'Request Password',
-        link: '/auth/request-password',
+        title: "Request Password",
+        link: "/auth/request-password"
       },
       {
-        title: 'Reset Password',
-        link: '/auth/reset-password',
-      },
-    ],
-  },
+        title: "Reset Password",
+        link: "/auth/reset-password"
+      }
+    ]
+  }
 ];
